eec866192393abfc627d47c5140cd88bbc752dd0
kzakfeld <kzakfeld@gmail.com>
2026-01-16 16:34:37 -0500

    Update IntakeIO.java

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,5 +1,29 @@
 package frc.robot.subsystems.intake;
 
-public interface IntakeIO {
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.Volts;
 
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+
+public interface IntakeIO {
+    @AutoLog
+    public static class IntakeIOInputs{
+        public boolean motorConnected = false;
+        public Angle motorPosition = Degrees.of(0);
+        public AngularVelocity motorVelocity = DegreesPerSecond.of(0.0);
+        public Voltage motorAppliedVolts = Volts.of(0.0);
+        public Current motorCurrent = Amps.of(0.0);
+        public Temperature motorTemp = Celsius.of(0.0);
+    }
+
+    public default void updateInputs(IntakeIOInputs inputs) {}
 }
