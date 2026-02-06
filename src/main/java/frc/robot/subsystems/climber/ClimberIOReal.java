d8520e44ba20ebd665cad2da7016be6fb2b50fd5
bbitsme <hascallblake71@gmail.com>
2026-02-06 18:33:14 -0500

    .

--- src/main/java/frc/robot/subsystems/climber/ClimberIOReal.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOReal.java
@@ -16,7 +16,6 @@ import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
-
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -25,90 +24,93 @@ import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
 
 public class ClimberIOReal implements ClimberIO {
-    //Motors
-    public final TalonFX climberMotor;
+  // Motors
+  public final TalonFX climberMotor;
 
-    //Conrtol Requests
-    private final VoltageOut voltageRequest = new VoltageOut(0);
+  // Conrtol Requests
+  private final VoltageOut voltageRequest = new VoltageOut(0);
 
-    //Status Signals
-    private final StatusSignal<Angle> climberPosition;
-    private final StatusSignal<AngularVelocity> climberVelocity;
-    private final StatusSignal<Voltage> climberVoltage;
-    private final StatusSignal<Current> climberCurrent;
-    private final StatusSignal<Temperature> climberTemperatre;
+  // Status Signals
+  private final StatusSignal<Angle> climberPosition;
+  private final StatusSignal<AngularVelocity> climberVelocity;
+  private final StatusSignal<Voltage> climberVoltage;
+  private final StatusSignal<Current> climberCurrent;
+  private final StatusSignal<Temperature> climberTemperatre;
 
-    //Debouncers
-    private final Debouncer motorConnectDebouncer = new Debouncer(.25);
+  // Debouncers
+  private final Debouncer motorConnectDebouncer = new Debouncer(.25);
 
-    public ClimberIOReal(){
-        climberMotor = new TalonFX(climberMotorCanID);
+  public ClimberIOReal() {
+    climberMotor = new TalonFX(climberMotorCanID);
 
-        TalonFXConfiguration climberConfiguration =
-      new TalonFXConfiguration()
-          .withAudio(
-              new AudioConfigs()
-                  .withAllowMusicDurDisable(enableBeeps)
-                  .withBeepOnBoot(enableBeeps)
-                  .withBeepOnConfig(enableBeeps))
-          .withCurrentLimits(
-              new CurrentLimitsConfigs()
-                  .withStatorCurrentLimit(climberStatorCurrentLimit)
-                  .withStatorCurrentLimitEnable(true)
-                  .withSupplyCurrentLimit(climberMotorSupplyLimitHigh)
-                  .withSupplyCurrentLowerLimit(climberMotorSupplyLimitLow)
-                  .withSupplyCurrentLowerTime(climberSupplyCurrentLowerTime)
-                  .withSupplyCurrentLimitEnable(true))
-          .withFeedback(
-              new FeedbackConfigs()
-                  .withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
-                  .withSensorToMechanismRatio(climberMotorReduction))
-          .withMotorOutput(
-              new MotorOutputConfigs()
-                  .withInverted(climberInverted)
-                  .withNeutralMode(climberNeutralMode))
-          .withSlot0(
-              new Slot0Configs()
-                  .withKP(climberKp)
-                  .withKI(climberKi)
-                  .withKD(climberKd)
-                  .withKG(0)
-                  .withKV(climberKv)
-                  .withKS(climberKs)
-                  .withKA(climberKa))
-          .withTorqueCurrent(
-              new TorqueCurrentConfigs()
-                  .withPeakForwardTorqueCurrent(climberStatorCurrentLimit)
-                  .withPeakReverseTorqueCurrent(climberStatorCurrentLimit.unaryMinus()));
+    TalonFXConfiguration climberConfiguration =
+        new TalonFXConfiguration()
+            .withAudio(
+                new AudioConfigs()
+                    .withAllowMusicDurDisable(enableBeeps)
+                    .withBeepOnBoot(enableBeeps)
+                    .withBeepOnConfig(enableBeeps))
+            .withCurrentLimits(
+                new CurrentLimitsConfigs()
+                    .withStatorCurrentLimit(climberStatorCurrentLimit)
+                    .withStatorCurrentLimitEnable(true)
+                    .withSupplyCurrentLimit(climberMotorSupplyLimitHigh)
+                    .withSupplyCurrentLowerLimit(climberMotorSupplyLimitLow)
+                    .withSupplyCurrentLowerTime(climberSupplyCurrentLowerTime)
+                    .withSupplyCurrentLimitEnable(true))
+            .withFeedback(
+                new FeedbackConfigs()
+                    .withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
+                    .withSensorToMechanismRatio(climberMotorReduction))
+            .withMotorOutput(
+                new MotorOutputConfigs()
+                    .withInverted(climberInverted)
+                    .withNeutralMode(climberNeutralMode))
+            .withSlot0(
+                new Slot0Configs()
+                    .withKP(climberKp)
+                    .withKI(climberKi)
+                    .withKD(climberKd)
+                    .withKG(0)
+                    .withKV(climberKv)
+                    .withKS(climberKs)
+                    .withKA(climberKa))
+            .withTorqueCurrent(
+                new TorqueCurrentConfigs()
+                    .withPeakForwardTorqueCurrent(climberStatorCurrentLimit)
+                    .withPeakReverseTorqueCurrent(climberStatorCurrentLimit.unaryMinus()));
 
-            tryUntilOk(5, ()-> climberMotor.getConfigurator().apply(climberConfiguration, 0.25));
+    tryUntilOk(5, () -> climberMotor.getConfigurator().apply(climberConfiguration, 0.25));
 
-        climberPosition = climberMotor.getPosition();
-        climberVelocity = climberMotor.getVelocity();
-        climberVoltage = climberMotor.getMotorVoltage();
-        climberCurrent = climberMotor.getStatorCurrent();
-        climberTemperatre = climberMotor.getDeviceTemp();
+    climberPosition = climberMotor.getPosition();
+    climberVelocity = climberMotor.getVelocity();
+    climberVoltage = climberMotor.getMotorVoltage();
+    climberCurrent = climberMotor.getStatorCurrent();
+    climberTemperatre = climberMotor.getDeviceTemp();
 
-        BaseStatusSignal.setUpdateFrequencyForAll(50.0, climberPosition, climberVelocity, climberVoltage, climberCurrent, climberTemperatre);
-        ParentDevice.optimizeBusUtilizationForAll(climberMotor);
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        50.0, climberPosition, climberVelocity, climberVoltage, climberCurrent, climberTemperatre);
+    ParentDevice.optimizeBusUtilizationForAll(climberMotor);
 
-        voltageRequest.EnableFOC = true;
-    }
+    voltageRequest.EnableFOC = true;
+  }
 
-    @Override
-    public void updateInputs(ClimberIOInputs inputs) {
-        var motorStatus = BaseStatusSignal.refreshAll(climberPosition, climberVelocity, climberVoltage, climberCurrent, climberTemperatre);
+  @Override
+  public void updateInputs(ClimberIOInputs inputs) {
+    var motorStatus =
+        BaseStatusSignal.refreshAll(
+            climberPosition, climberVelocity, climberVoltage, climberCurrent, climberTemperatre);
 
-        inputs.motorConnected = motorConnectDebouncer.calculate(motorStatus.isOK());
-        inputs.motorPosition = climberPosition.getValue();
-        inputs.motorVelocity = climberVelocity.getValue();
-        inputs.motorAppliedVolts = climberVoltage.getValue();
-        inputs.motorCurrent = climberCurrent.getValue();
-        inputs.motorTemp = climberTemperatre.getValue();
-    }
+    inputs.motorConnected = motorConnectDebouncer.calculate(motorStatus.isOK());
+    inputs.motorPosition = climberPosition.getValue();
+    inputs.motorVelocity = climberVelocity.getValue();
+    inputs.motorAppliedVolts = climberVoltage.getValue();
+    inputs.motorCurrent = climberCurrent.getValue();
+    inputs.motorTemp = climberTemperatre.getValue();
+  }
 
-    @Override
-    public void setClimber(Voltage volts) {
-        climberMotor.setControl(voltageRequest.withOutput(volts));
-    }
+  @Override
+  public void setClimber(Voltage volts) {
+    climberMotor.setControl(voltageRequest.withOutput(volts));
+  }
 }
