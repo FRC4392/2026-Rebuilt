80873a56ffb30ec100c51d65fcd365312edce185
bbitsme <hascallblake71@gmail.com>
2026-02-06 18:35:27 -0500

    .

--- src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
@@ -16,7 +16,6 @@ import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
-
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -25,91 +24,93 @@ import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
 
 public class IntakeIOReal implements IntakeIO {
-    //Motors
-    private final TalonFX intakeMotor;
+  // Motors
+  private final TalonFX intakeMotor;
 
-    //Control Requests
-    private final VoltageOut voltageRequest = new VoltageOut(0);
+  // Control Requests
+  private final VoltageOut voltageRequest = new VoltageOut(0);
 
-    //Status Signals
-    private final StatusSignal<Angle> intakePosition;
-    private final StatusSignal<AngularVelocity> intakeVelocity;
-    private final StatusSignal<Voltage> intakeVoltage;
-    private final StatusSignal<Current> intakeCurrent;
-    private final StatusSignal<Temperature> intakeTemperature;
+  // Status Signals
+  private final StatusSignal<Angle> intakePosition;
+  private final StatusSignal<AngularVelocity> intakeVelocity;
+  private final StatusSignal<Voltage> intakeVoltage;
+  private final StatusSignal<Current> intakeCurrent;
+  private final StatusSignal<Temperature> intakeTemperature;
 
-    //Debouncers
-    private final Debouncer motorConnectedDebouncer = new Debouncer(.25);
+  // Debouncers
+  private final Debouncer motorConnectedDebouncer = new Debouncer(.25);
 
-    public IntakeIOReal(){
-        intakeMotor = new TalonFX(intakeMotorCanID);
+  public IntakeIOReal() {
+    intakeMotor = new TalonFX(intakeMotorCanID);
 
-        TalonFXConfiguration intakeConfiguration =
-      new TalonFXConfiguration()
-          .withAudio(
-              new AudioConfigs()
-                  .withAllowMusicDurDisable(enableBeeps)
-                  .withBeepOnBoot(enableBeeps)
-                  .withBeepOnConfig(enableBeeps))
-          .withCurrentLimits(
-              new CurrentLimitsConfigs()
-                  .withStatorCurrentLimit(intakeStatorCurrentLimit)
-                  .withStatorCurrentLimitEnable(true)
-                  .withSupplyCurrentLimit(intakeMotorSupplyLimitHigh)
-                  .withSupplyCurrentLowerLimit(intakeMotorSupplyLimitLow)
-                  .withSupplyCurrentLowerTime(intakeSupplyCurrentLowerTime)
-                  .withSupplyCurrentLimitEnable(true))
-          .withFeedback(
-              new FeedbackConfigs()
-                  .withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
-                  .withSensorToMechanismRatio(intakeMotorReduction))
-          .withMotorOutput(
-              new MotorOutputConfigs()
-                  .withInverted(intakeInverted)
-                  .withNeutralMode(intakeNeutralMode))
-          .withSlot0(
-              new Slot0Configs()
-                  .withKP(intakeKp)
-                  .withKI(intakeKi)
-                  .withKD(intakeKd)
-                  .withKG(0)
-                  .withKV(intakeKv)
-                  .withKS(intakeKs)
-                  .withKA(intakeKa))
-          .withTorqueCurrent(
-              new TorqueCurrentConfigs()
-                  .withPeakForwardTorqueCurrent(intakeStatorCurrentLimit)
-                  .withPeakReverseTorqueCurrent(intakeStatorCurrentLimit.unaryMinus()));
+    TalonFXConfiguration intakeConfiguration =
+        new TalonFXConfiguration()
+            .withAudio(
+                new AudioConfigs()
+                    .withAllowMusicDurDisable(enableBeeps)
+                    .withBeepOnBoot(enableBeeps)
+                    .withBeepOnConfig(enableBeeps))
+            .withCurrentLimits(
+                new CurrentLimitsConfigs()
+                    .withStatorCurrentLimit(intakeStatorCurrentLimit)
+                    .withStatorCurrentLimitEnable(true)
+                    .withSupplyCurrentLimit(intakeMotorSupplyLimitHigh)
+                    .withSupplyCurrentLowerLimit(intakeMotorSupplyLimitLow)
+                    .withSupplyCurrentLowerTime(intakeSupplyCurrentLowerTime)
+                    .withSupplyCurrentLimitEnable(true))
+            .withFeedback(
+                new FeedbackConfigs()
+                    .withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
+                    .withSensorToMechanismRatio(intakeMotorReduction))
+            .withMotorOutput(
+                new MotorOutputConfigs()
+                    .withInverted(intakeInverted)
+                    .withNeutralMode(intakeNeutralMode))
+            .withSlot0(
+                new Slot0Configs()
+                    .withKP(intakeKp)
+                    .withKI(intakeKi)
+                    .withKD(intakeKd)
+                    .withKG(0)
+                    .withKV(intakeKv)
+                    .withKS(intakeKs)
+                    .withKA(intakeKa))
+            .withTorqueCurrent(
+                new TorqueCurrentConfigs()
+                    .withPeakForwardTorqueCurrent(intakeStatorCurrentLimit)
+                    .withPeakReverseTorqueCurrent(intakeStatorCurrentLimit.unaryMinus()));
 
-        tryUntilOk(5, ()->intakeMotor.getConfigurator().apply(intakeConfiguration, 0.25));
+    tryUntilOk(5, () -> intakeMotor.getConfigurator().apply(intakeConfiguration, 0.25));
 
-        intakePosition = intakeMotor.getPosition();
-        intakeVelocity = intakeMotor.getVelocity();
-        intakeVoltage = intakeMotor.getMotorVoltage();
-        intakeCurrent = intakeMotor.getStatorCurrent();
-        intakeTemperature = intakeMotor.getDeviceTemp();
+    intakePosition = intakeMotor.getPosition();
+    intakeVelocity = intakeMotor.getVelocity();
+    intakeVoltage = intakeMotor.getMotorVoltage();
+    intakeCurrent = intakeMotor.getStatorCurrent();
+    intakeTemperature = intakeMotor.getDeviceTemp();
 
-        BaseStatusSignal.setUpdateFrequencyForAll(50.0, intakePosition, intakeVelocity, intakeVoltage, intakeCurrent, intakeTemperature);
-        ParentDevice.optimizeBusUtilizationForAll(intakeMotor);
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        50.0, intakePosition, intakeVelocity, intakeVoltage, intakeCurrent, intakeTemperature);
+    ParentDevice.optimizeBusUtilizationForAll(intakeMotor);
 
-        voltageRequest.EnableFOC = true;
-    }
+    voltageRequest.EnableFOC = true;
+  }
 
-    @Override
-    public void updateInputs(IntakeIOInputs inputs){
-        var motorStatus = BaseStatusSignal.refreshAll(intakePosition, intakeVelocity, intakeVoltage, intakeCurrent, intakeTemperature);
+  @Override
+  public void updateInputs(IntakeIOInputs inputs) {
+    var motorStatus =
+        BaseStatusSignal.refreshAll(
+            intakePosition, intakeVelocity, intakeVoltage, intakeCurrent, intakeTemperature);
 
-        inputs.motorConnected = motorConnectedDebouncer.calculate(motorStatus.isOK());
-        inputs.motorPosition = intakePosition.getValue();
-        inputs.motorVelocity = intakeVelocity.getValue();
-        inputs.motorAppliedVolts = intakeVoltage.getValue();
-        inputs.motorCurrent = intakeCurrent.getValue();
-        inputs.motorTemp = intakeTemperature.getValue();
-    }
+    inputs.motorConnected = motorConnectedDebouncer.calculate(motorStatus.isOK());
+    inputs.motorPosition = intakePosition.getValue();
+    inputs.motorVelocity = intakeVelocity.getValue();
+    inputs.motorAppliedVolts = intakeVoltage.getValue();
+    inputs.motorCurrent = intakeCurrent.getValue();
+    inputs.motorTemp = intakeTemperature.getValue();
+  }
 
-    @Override
-    public void setIntake(Voltage volts) {
-        intakeMotor.setControl(voltageRequest.withOutput(volts));
-    }
-
+  @Override
+  public void setIntake(Voltage volts) {
+    intakeMotor.setControl(voltageRequest.withOutput(volts));
+  }
 }
